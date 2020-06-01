7cf344e940c58ccc3d3517137493267eccc7ffc9
cloudgang <35246400+CloudGang@users.noreply.github.com>
2020-05-31 22:17:07 -0400

    Update Character.js

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,22 @@
 // Write your Character component here
+import React from 'react';
+
+const Character = (props) => (
+
+  <li className="cast">
+
+    <h3>
+
+      <span>{props.name}</span>
+      <span>{props.gender}</span>
+      <span>{props.height}</span>
+      <span>{props.eye_color}</span>
+      <span>{props.birth_year}</span>
+
+    </h3>
+
+  </li>
+  
+);
+
+export default Character;
